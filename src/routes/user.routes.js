c8500483f83f34c7184f16870c2a8fcf58768061
adityaprajapati10 <adityababuprajapati@gmail.com>
2025-01-23 14:45:43 +0530

    add register controller and bug fixing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
 import { asyncHandler } from "../utils/asyncHandler.js"; 
+
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router()
 
-router.route("/register").post(asyncHandler(registerUser))
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
 
 export default router
